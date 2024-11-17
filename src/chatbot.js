7ae3583a630121f472a273f8c6d731f71d51857e
Mahak <mahakyadav0520@gmail.com>
2024-11-17 15:14:16 +0530

    added

--- src/chatbot.js
+++ src/chatbot.js
@@ -15,7 +15,7 @@ const Chatbot = () => {
 
         try {
             // Call backend chatbot API
-            const response = await fetch("http://127.0.0.1:5000/chat", {
+            const response = await fetch("https://flask-chatbot-latest.onrender.com", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ user_input: userInput }),
